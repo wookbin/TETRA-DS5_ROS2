fb2736d4ef28eced371c5bb1ce543e26eabf9104
Wookbin Jin <58063370+wookbin@users.noreply.github.com>
2023-10-11 10:55:36 +0900

    Add files via upload

---
+++ iahrs_driver/launch/iahrs_driver.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+
+import os
+
+from launch import LaunchDescription
+from launch.actions import ExecuteProcess, IncludeLaunchDescription
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+from launch.substitutions import LaunchConfiguration
+
+from launch_ros.actions import Node
+
+# this is the function launch  system will look for
+def generate_launch_description():
+
+    iahrs_driver_node = Node(
+        package='iahrs_driver', 
+        executable='iahrs_driver',
+        output='screen',
+        parameters=[
+            {"m_bSingle_TF_option": True}
+        ]
+    )
+
+    # create and return launch description object
+    return LaunchDescription(
+        [
+            iahrs_driver_node
+        ]
+    )
