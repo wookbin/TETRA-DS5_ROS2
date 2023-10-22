253ea2b483197e349c45f1dc2bea16fe0b0b2973
Wookbin Jin <58063370+wookbin@users.noreply.github.com>
2023-10-23 08:33:47 +0900

    Add files via upload

---
+++ tetra/launch/tetra.launch.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+
+import os
+
+from launch import LaunchDescription
+from launch.actions import ExecuteProcess, IncludeLaunchDescription
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+from launch.substitutions import LaunchConfiguration
+
+from launch_ros.actions import Node
+
+# this is the function launch  system will look for
+def generate_launch_description():
+
+    tetra_node = Node(
+        package='tetra', 
+        executable='tetra',
+        output='screen',
+        parameters=[
+            {"m_bEKF_option": False}
+        ]
+    )
+
+    # create and return launch description object
+    return LaunchDescription(
+        [
+            tetra_node
+        ]
+    )
