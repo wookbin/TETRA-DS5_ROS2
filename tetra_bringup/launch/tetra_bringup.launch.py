dbd14151b4fd11868c0cce22b4a46edb27191e31
Wookbin Jin <58063370+wookbin@users.noreply.github.com>
2025-02-05 15:19:23 +0900

    Add files via upload

---
+++ tetra_bringup/launch/tetra_bringup.launch.py
@@ -0,0 +1,145 @@
+#!/usr/bin/env python3
+
+import os
+import yaml
+
+from ament_index_python.packages import get_package_share_directory
+from launch import LaunchDescription
+from launch.actions import ExecuteProcess, IncludeLaunchDescription, DeclareLaunchArgument
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+from launch.substitutions import PathJoinSubstitution, Command, LaunchConfiguration
+from launch_ros.substitutions import FindPackageShare
+from launch_ros.actions import Node
+
+
+# this is the function launch  system will look for
+def generate_launch_description():
+
+    # tetra Motor Driver Board
+    tetra_node = Node(
+        package='tetra', 
+        executable='tetra',
+        output='screen',
+        parameters=[
+            {"m_bEKF_option": True} #default: False
+        ]
+    )
+    
+    # EKF Localization
+    ekf_localization_node= Node(
+            package='robot_localization',
+            executable='ekf_node',
+            name='ekf_filter_node',
+            output='screen',
+            parameters=[os.path.join(get_package_share_directory("tetra_bringup"), 'params', 'ekf.yaml')],
+            arguments=['--ros-args', '--log-level', 'error']
+            #remappings=[('/odometry/filtered', '/odom')],
+    )
+    
+    # tetra_interface Board
+    tetra_interface_node = Node(
+        package='tetra_interface', 
+        executable='tetra_interface',
+        output='screen',
+        parameters=[
+            {"m_bConveyor_option": False},
+            {"m_bUltrasonic_option": False}
+        ]
+    )
+    
+    # IMU Sensor
+    iahrs_driver_node = Node(
+        package='iahrs_driver', 
+        executable='iahrs_driver',
+        output='screen',
+        parameters=[
+            {"m_bSingle_TF_option": False} # default: True
+        ]
+    )
+    
+    # Joystick
+    joy_node = Node(
+        package='joy', 
+        executable='joy_node',
+        output='screen',
+        parameters=[
+            {"deadzone": 0.05}
+        ]
+    )
+    
+    # tetra_URDF
+    use_sim_time = DeclareLaunchArgument('use_sim_time', default_value="false")
+    rsp_node = Node(
+        package='robot_state_publisher',
+        executable='robot_state_publisher',
+        name='robot_state_publisher',
+        output='screen',
+        parameters=[{
+            'use_sim_time': LaunchConfiguration('use_sim_time'),
+            'robot_description':
+                Command([
+                    'xacro ',
+                    PathJoinSubstitution([
+                        FindPackageShare('tetra_description'),
+                        'urdf',
+                        'tetra.xacro',
+                    ]),
+                ]),
+        }]
+    )
+    
+    # tetra_service
+    tetra_service_node = Node(
+        package='tetra_service', 
+        executable='tetra_service',
+        output='screen',
+        parameters=[
+            {"m_dHome_ID": 0}
+        ]
+    )
+    
+    
+    # create and return launch description object
+    return LaunchDescription(
+        [
+            tetra_node,
+            ekf_localization_node,
+            tetra_interface_node, 
+            iahrs_driver_node,
+            joy_node,
+            use_sim_time,
+            rsp_node,
+            tetra_service_node,
+            
+		# USB Camera
+		IncludeLaunchDescription(
+		PythonLaunchDescriptionSource(
+			[get_package_share_directory('usb_cam'), '/launch/camera.launch.py']),
+		),
+        
+        # apriltag_ros
+		IncludeLaunchDescription(
+		PythonLaunchDescriptionSource(
+			[get_package_share_directory('apriltag_ros'), '/launch/apriltag_detection.launch.py']),
+		),
+			
+		# sick_tim_571
+		IncludeLaunchDescription(
+		PythonLaunchDescriptionSource(
+			[get_package_share_directory('sick_scan_xd'), '/launch/sick_tim_5xx.launch.py']),
+		),
+		
+		# cygbot 2D lidar
+		IncludeLaunchDescription(
+		PythonLaunchDescriptionSource(
+			[get_package_share_directory('cyglidar_d1_ros2'), '/launch/cyglidar.launch.py']),
+		),
+		
+		# realsense D455
+		IncludeLaunchDescription(
+		PythonLaunchDescriptionSource(
+			[get_package_share_directory('realsense2_camera'), '/launch/rs_launch.py']),
+		),
+        
+        ]
+    )
---
+++ tetra_cartographer/launch/cartographer.launch.py
@@ -0,0 +1,88 @@
+
+import os
+from ament_index_python.packages import get_package_share_directory
+from launch import LaunchDescription
+from launch.actions import DeclareLaunchArgument
+from launch_ros.actions import Node
+from launch.substitutions import LaunchConfiguration
+from launch.actions import IncludeLaunchDescription
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+from launch.substitutions import ThisLaunchFileDir
+
+
+def generate_launch_description():
+    use_sim_time = LaunchConfiguration('use_sim_time', default='true')
+    turtlebot3_cartographer_prefix = get_package_share_directory('tetra_cartographer')
+    cartographer_config_dir = LaunchConfiguration('cartographer_config_dir', default=os.path.join(
+                                                  turtlebot3_cartographer_prefix, 'config'))
+    configuration_basename = LaunchConfiguration('configuration_basename',
+                                                 default='tetra_lds_2d.lua')
+
+    resolution = LaunchConfiguration('resolution', default='0.05')
+    publish_period_sec = LaunchConfiguration('publish_period_sec', default='1.0')
+
+    rviz_config_dir = os.path.join(get_package_share_directory('tetra_cartographer'),
+                                   'rviz', 'tetra_cartographer.rviz')
+
+
+    return LaunchDescription([
+        DeclareLaunchArgument(
+            'cartographer_config_dir',
+            default_value=cartographer_config_dir,
+            description='Full path to config file to load'),
+        DeclareLaunchArgument(
+            'configuration_basename',
+            default_value=configuration_basename,
+            description='Name of lua file for cartographer'),
+        DeclareLaunchArgument(
+            'use_sim_time',
+            default_value='false',
+            description='Use simulation (Gazebo) clock if true'),
+
+        Node(
+            package='cartographer_ros',
+            executable='cartographer_node',
+            name='cartographer_node',
+            output='screen',
+            parameters=[{'use_sim_time': use_sim_time}],
+            arguments=['-configuration_directory', cartographer_config_dir,
+                       '-configuration_basename', configuration_basename],
+	      remappings=[
+		('echoes', 'scan'),
+		('/odom', '/odometry/filtered')
+	      ]
+	    ),
+
+        DeclareLaunchArgument(
+            'resolution',
+            default_value=resolution,
+            description='Resolution of a grid cell in the published occupancy grid'),
+
+        DeclareLaunchArgument(
+            'publish_period_sec',
+            default_value=publish_period_sec,
+            description='OccupancyGrid publishing period'),
+
+        IncludeLaunchDescription(
+            PythonLaunchDescriptionSource([ThisLaunchFileDir(), '/occupancy_grid.launch.py']),
+            launch_arguments={'use_sim_time': use_sim_time, 'resolution': resolution,
+                              'publish_period_sec': publish_period_sec}.items(),
+        ),
+
+            
+        Node(
+            package='rviz2',
+            executable='rviz2',
+            name='rviz2',
+            arguments=['-d', rviz_config_dir],
+            parameters=[{'use_sim_time': use_sim_time}],
+            output='screen'),
+            
+      Node(
+    	package='tf2_ros',
+    	executable='static_transform_publisher',
+   	name='link1_broadcaster',
+    	arguments=['0', '0', '0', '0', '0', '0', '1', 'map', 'odom'],
+    	output='screen'),
+            
+    ])
